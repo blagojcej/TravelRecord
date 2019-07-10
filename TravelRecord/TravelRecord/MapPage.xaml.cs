cf2ca5e2078ce74a9b9b5396c94d6d8060a63a7e
Blagojce Jovanoski <blagojcejovanoski@yahoo.com>
2019-07-10 15:19:58 +0200

    Finished implementation maps into the application.

--- TravelRecord/TravelRecord/MapPage.xaml.cs
+++ TravelRecord/TravelRecord/MapPage.xaml.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Plugin.Geolocator;
 using Xamarin.Forms;
+using Xamarin.Forms.Maps;
 using Xamarin.Forms.Xaml;
 
 namespace TravelRecord
@@ -16,5 +17,32 @@ namespace TravelRecord
 		{
 			InitializeComponent ();
 		}
-	}
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var locator = CrossGeolocator.Current;
+
+            //Know when location changed
+            locator.PositionChanged += Locator_PositionChanged;
+            await locator.StartListeningAsync(new TimeSpan(0), 100);
+
+            //get current position
+            var position = await locator.GetPositionAsync();
+
+            var center = new Position(position.Latitude, position.Longitude);
+            //display in the center (1 degree to the right and 1 degree to the left) - first number 2, (1 degree from the top and 1 degree from the bottom) - second number 2
+            var span = new MapSpan(center, 2, 2);
+            locationsMap.MoveToRegion(span);
+        }
+
+        private void Locator_PositionChanged(object sender, Plugin.Geolocator.Abstractions.PositionEventArgs e)
+        {
+            var center = new Position(e.Position.Latitude, e.Position.Longitude);
+            //display in the center (1 degree to the right and 1 degree to the left) - first number 2, (1 degree from the top and 1 degree from the bottom) - second number 2
+            var span = new MapSpan(center, 2, 2);
+            locationsMap.MoveToRegion(span);
+        }
+    }
 }
