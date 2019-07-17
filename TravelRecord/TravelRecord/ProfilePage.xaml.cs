a22f9aa1081c02f66afe24e1b11978f3aa21c4ac
Blagojce Jovanoski <blagojcejovanoski@yahoo.com>
2019-07-17 12:33:03 +0200

    Design Profile Page using LINQ to get data from database,

--- TravelRecord/TravelRecord/ProfilePage.xaml.cs
+++ TravelRecord/TravelRecord/ProfilePage.xaml.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using SQLite;
+using TravelRecord.Model;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -16,5 +17,43 @@ namespace TravelRecord
 		{
 			InitializeComponent ();
 		}
-	}
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            using (SQLiteConnection conn=new SQLiteConnection(App.DatabaseLocation))
+            {
+                //Get post table and list all posts in table
+                var postTable = conn.Table<Post>().ToList();
+
+                //Get all distinct categories using LINQ
+                var categories = (from p in postTable
+                    orderby p.CategoryId
+                    select p.CategoryName).Distinct().ToList();
+
+                //Another way to get all distinct categories using LINQ
+                var categories2 = postTable.OrderBy(p => p.CategoryId).Select(p => p.CategoryName).Distinct().ToList();
+
+                //Get amount of posts by category using LINQ
+                Dictionary<string, int> categoriesCount=new Dictionary<string, int>();
+                foreach (var category in categories)
+                {
+                    var count = (from post in postTable
+                        where post.CategoryName == category
+                        select post).ToList().Count;
+
+                    //Another way to get amount of posts by category using LINQ
+                    var count2 = postTable.Where(p => p.CategoryName == category).ToList().Count;
+
+                    categoriesCount.Add(category, count);
+                }
+
+                categoriesListView.ItemsSource = categoriesCount;
+
+                postCountLabel.Text = postTable.Count.ToString();
+
+            }
+        }
+    }
 }
